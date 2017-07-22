e4eb8e5793b4756978c825c599c20fdacf81c52a
Amrong Lenon Long <amronglong@gmail.com>
2017-07-22 11:05:11 -0700

    Completed the reveal on scroll behavior.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,11 @@
+/*jshint esversion: 6 */
 // import  __name whatever you want___ from  ' type path '
 
-import Mobilemenu from  './modules/Mobilemenu';
+import Mobilemenu     from  './modules/Mobilemenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 // save new object to variable
-
-var mobileMenu = new Mobilemenu();
+var mobileMenu     = new Mobilemenu();
+new RevealOnScroll($('.feature-item'), '75%');
+new RevealOnScroll($('.testimonial'), '60%');
