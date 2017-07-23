321ad4e26ef719174c6cc7502f49aa1b151aa068
Amrong Lenon Long <amronglong@gmail.com>
2017-07-23 14:59:15 -0700

    Completed sticky header functionality.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,13 @@
 /*jshint esversion: 6 */
 // import  __name whatever you want___ from  ' type path '
 
-import Mobilemenu     from  './modules/Mobilemenu';
+import Mobilemenu     from './modules/Mobilemenu';
 import RevealOnScroll from './modules/RevealOnScroll';
-import $ from 'jquery';
+import $              from 'jquery';
+import StickyHeader   from './modules/StickyHeader';
 
 // save new object to variable
 var mobileMenu     = new Mobilemenu();
-new RevealOnScroll($('.feature-item'), '75%');
+new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonial'), '60%');
+var stickyHeader = new StickyHeader();
