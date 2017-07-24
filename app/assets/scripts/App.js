f0728b724a26604b05e667ef90caad95100c91d9
Amrong Lenon Long <amronglong@gmail.com>
2017-07-24 12:37:59 -0700

    Completed the modal overlay.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,9 +5,11 @@ import Mobilemenu     from './modules/Mobilemenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $              from 'jquery';
 import StickyHeader   from './modules/StickyHeader';
+import Modal          from './modules/Modal';
 
 // save new object to variable
 var mobileMenu     = new Mobilemenu();
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonial'), '60%');
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,41 @@
+import $ from 'jquery';
+
+class Modal {
+  constructor() {
+    this.openModalButton = $(".open-modal");
+    this.modal = $('.modal');
+    this.closeModalButton = $(".modal__close");
+    this.events();
+  }
+
+// remember to call events();
+events() {
+  // clicking the open modal button  //.bind(this)keyword will point to our property
+  this.openModalButton.click(this.openModal.bind(this));
+
+  // clicking the x closed button
+  this.closeModalButton.click(this.closeModal.bind(this));
+
+  // pushes the esq key
+  $(document).keyup(this.keyPressHandler.bind(this));
+}
+
+
+keyPressHandler(e) {
+  if (e.keyCode == 27) {
+    this.modal.removeClass("modal--is-visible");
+  }
+}
+// Methods
+openModal() {
+  this.modal.addClass("modal--is-visible");
+  // return false prevents the clicking of "Get-in-touch" button from scrolling to the top
+  return false;
+}
+
+closeModal() {
+  this.modal.removeClass("modal--is-visible");
+  }
+}
+
+export default Modal;
